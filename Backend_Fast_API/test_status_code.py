5adfd07d23dcb742f306ebb9216be22cd7837c00
SaidaAourras <sa123aouras@gmail.com>
2025-10-31 10:42:05 +0100

    readme.md

--- Backend_Fast_API/test_status_code.py
+++ Backend_Fast_API/test_status_code.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
 from main import app
 
-cleint = TestClient(app)
+client = TestClient(app)
 def test_status_code():
-    response = cleint.get('/patients/predict_risk')
+    response = client.get('/patients/predict_risk')
     # print(response.status_code)
     assert response.status_code == 200
